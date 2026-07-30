0af2ed0e162290991b3f3c4f98095f7d8d7b2b62
gichigig <billyblund007@gmail.com>
2026-07-30 04:05:11 +0300

    Add safety standards page and update contact email

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         privacy: resolve(__dirname, 'privacy.html'),
+        safety: resolve(__dirname, 'safety-standards.html'),
         delete: resolve(__dirname, 'delete-account.html')
       }
     }
