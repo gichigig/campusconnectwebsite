008cb286c720f2604711b56b54519a8da9a18fee
gichigig <billyblund007@gmail.com>
2026-07-30 04:02:18 +0300

    Add vite config to build multiple pages

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        delete: resolve(__dirname, 'delete-account.html')
+      }
+    }
+  }
+})
